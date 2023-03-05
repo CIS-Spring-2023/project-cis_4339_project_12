e34c95bf9a8f2651520e7b1d280f48430d72d446
Ayman Ahmed <98271654+aymanzak19@users.noreply.github.com>
2023-03-04 22:18:31 -0600

    Add files via upload
    Login component to create a login page with user roles

---
+++ frontend/src/components/login.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="flex justify-center items-center h-screen bg-gray-50 text-xl" >
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="login">
+        <div class="mb-4">
+          <label class="block text-gray-700 font-bold mb-2" for="username">
+            Username
+          </label>
+          <input
+            v-model="username"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div class="mb-6">
+          <label class="block text-gray-700 font-bold mb-2" for="password">
+            Password
+          </label>
+          <input
+            v-model="password"
+            class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="********"
+          />
+          <p class="text-red-500 text-xs italic">{{ errorMessage }}</p>
+        </div>
+
+        <div>
+          <!--
+            https://getbootstrap.com/docs/5.0/forms/checks-radios/
+            radio icon code taken from Bootstrap
+          -->
+          <div class="form-check">
+            <input class="form-check-input" v-model="viewer" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+            <label class="form-check-label" for="flexRadioDefault1">
+              Viewer
+            </label>
+          </div>
+          <div class="form-check">
+            <input class="form-check-input" v-model="editor" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+              Editor
+            </label>
+          </div>
+
+
+        </div>
+        <br>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+   
+  
+  <script>
+  import axios from "axios";
+  const apiURL = import.meta.env.VITE_ROOT_API;
+  export default {
+    name: "LoginPage",
+    data() {
+      return {
+        username: "",
+        password: "",
+        errorMessage: "",
+      };
+    },
+    methods: {
+      login() {
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        axios
+          .post(`${apiURL}/login`, userData)
+          .then((res) => {
+            // save token and user role to localStorage or Vuex store
+            localStorage.setItem("token", res.data.token);
+            // check if user is a viewer or editor
+            if (res.data.role === "viewer" || res.data.role === "editor") {
+              localStorage.setItem("role", res.data.role);
+              // redirect to dashboard
+              this.$router.push("/");
+            } else {
+              this.errorMessage = "Invalid role";
+            }
+          })
+          .catch((err) => {
+            this.errorMessage = err.response.data.message;
+          });
+      },
+    },
+  };
+  </script>
+  
+   
+  
+  <style>
+  /* styles for login page */
+  </style>
+  
