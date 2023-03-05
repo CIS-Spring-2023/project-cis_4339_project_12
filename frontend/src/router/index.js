f59765f6f0da672b9a5bc47bd67402c212d8a6f1
areeshafarooq <106890729+areeshafarooq@users.noreply.github.com>
2023-03-05 12:04:46 -0600

    Updated the index.js file and imported components.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,48 +1,86 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from '../components/login.vue'
+import HomePage from '../components/homePage.vue'
+import IntakeForm from '../components/intakeForm.vue'
+import FindClient from '../components/findClient.vue'
+import UpdateClient from '../components/updateClient.vue'
+import EventForm from '../components/eventForm.vue'
+import FindEvents from '../components/findEvents.vue'
+import EventDetails from '../components/eventDetails.vue'
+import Services from '../components/services.vue'
 
 // make all paths and names lowercase for consistency
 const routes = [
   {
     path: '/',
     props: true,
-    component: () => import('../components/homePage.vue')
+    component: HomePage
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginPage
   },
   {
     path: '/intakeform',
     name: 'intakeform',
-    props: true,
-    component: () => import('../components/intakeForm.vue')
+    //meta: { requiresAuth: true },
+    component: IntakeForm
   },
   {
     path: '/findclient',
     name: 'findclient',
-    component: () => import('../components/findClient.vue')
+    //meta: { requiresAuth: true },
+    component: FindClient
   },
   {
     path: '/updateclient/:id',
     name: 'updateclient',
+    //meta: { requiresAuth: true },
     props: true,
-    component: () => import('../components/updateClient.vue')
+    component: UpdateClient
   },
   {
     path: '/eventform',
     name: 'eventform',
-    component: () => import('../components/eventForm.vue')
+    //meta: { requiresAuth: true },
+    component: EventForm
   },
   {
     path: '/findevents',
     name: 'findevents',
-    component: () => import('../components/findEvents.vue')
+    //meta: { requiresAuth: true },
+    component: FindEvents
   },
   {
     path: '/eventdetails/:id',
     name: 'eventdetails',
+    //meta: { requiresAuth: true },
     props: true,
-    component: () => import('../components/eventDetails.vue')
+    component: EventDetails
+  },
+  {
+    path: '/services',
+    name: 'services',
+    //meta: { requiresAuth: true },
+    props: true,
+    component: Services
   }
 ]
+ 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+ 
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const isAuthenticated = localStorage.getItem('user') // check if the user is authenticated
+  if (requiresAuth && !isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+ 
 export default router
