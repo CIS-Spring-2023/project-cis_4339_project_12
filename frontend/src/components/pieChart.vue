790f9b6c8247173e507ce77e20f79c4ef655f586
Ayman Ahmed <98271654+aymanzak19@users.noreply.github.com>
2023-04-26 21:01:05 -0500

    moved donought chart code to its own file

---
+++ frontend/src/components/pieChart.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="flex justify-center">
+      <div class="w-full ml-4">
+        <div class="shadow-lg rounded-lg overflow-hidden">
+          <canvas class="p-10" ref="zipCodeChart"></canvas>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+ 
+
+<script>
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
+
+ 
+
+export default {
+  props: {
+    chartData: {
+      type: Array
+    }
+  },
+  async mounted() {
+    await new Chart(this.$refs.zipCodeChart, {
+      type: 'doughnut',
+      data: {
+        labels: this.chartData.map(item => item.zipCode),
+        datasets: [
+          {
+            borderWidth: 1,
+            backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745', '#6c757d'],
+            borderColor: '#fff',
+            data: this.chartData.map(item => item.count)
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: true
+      }
+    })
+  },
+  methods: {
+    getColor() {
+      let channel = () => Math.random() * 255
+      return `rgba(${channel()}, ${channel()}, ${channel()}, 0.2)`
+    }
+  }
+}
+</script>
