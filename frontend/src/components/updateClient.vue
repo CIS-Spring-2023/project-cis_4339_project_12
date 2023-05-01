4a856579be3f891faff8b315348ed38aa86b4fb3
areeshafarooq <106890729+areeshafarooq@users.noreply.github.com>
2023-04-30 21:27:33 -0500

    Add files via upload
    Added functions to connect to the backend CRUD operations

--- frontend/src/components/updateClient.vue
+++ frontend/src/components/updateClient.vue
@@ -91,19 +91,17 @@ export default {
           .put(`${apiURL}/events/register`, null, {
             params: { event: event._id, client: this.id }
           })
-          .then(() => this.getEventsRegistered())
+          .then(() => { this.getEventsRegistered(); alert('Client Added') })
           .catch((error) => {
             if (error.response.data) {
               alert(`${event.name}: ${error.response.data}`)
             }
           })
+
       })
       // clear events selection after attempting to register for events
       this.eventsSelected = []
     },
-    // replaces client hard delete
-    // find all events where client appears in attendees array and pull it
-    // then pull org from client org array
     deregisterClient() {
       axios
         .get(`${apiURL}/events/client/${this.id}`)
@@ -149,36 +147,25 @@ export default {
 </script>
 <template>
   <main>
-    <h1
-      class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
-    >
+    <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
       Update Client
     </h1>
     <div class="px-10 py-20">
       <!-- @submit.prevent stops the submit event from reloading the page-->
       <form @submit.prevent="handleSubmitForm">
         <!-- grid container -->
-        <div
-          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-        >
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
           <h2 class="text-2xl font-bold">Personal Details</h2>
           <!-- form field -->
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">First Name</span>
               <span style="color: #ff0000">*</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="client.firstName"
-              />
+                placeholder v-model="client.firstName" />
               <span class="text-black" v-if="v$.client.firstName.$error">
-                <p
-                  class="text-red-700"
-                  v-for="error of v$.client.firstName.$errors"
-                  :key="error.$uid"
-                >
+                <p class="text-red-700" v-for="error of v$.client.firstName.$errors" :key="error.$uid">
                   {{ error.$message }}!
                 </p>
               </span>
@@ -189,12 +176,9 @@ export default {
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Middle Name</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="client.middleName"
-              />
+                placeholder v-model="client.middleName" />
             </label>
           </div>
 
@@ -203,18 +187,11 @@ export default {
             <label class="block">
               <span class="text-gray-700">Last Name</span>
               <span style="color: #ff0000">*</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                placeholder
-                v-model="client.lastName"
-              />
+                placeholder v-model="client.lastName" />
               <span class="text-black" v-if="v$.client.lastName.$error">
-                <p
-                  class="text-red-700"
-                  v-for="error of v$.client.lastName.$errors"
-                  :key="error.$uid"
-                >
+                <p class="text-red-700" v-for="error of v$.client.lastName.$errors" :key="error.$uid">
                   {{ error.$message }}!
                 </p>
               </span>
@@ -225,18 +202,11 @@ export default {
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Email</span>
-              <input
-                type="email"
+              <input type="email"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                v-model="client.email"
-              />
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" v-model="client.email" />
               <span class="text-black" v-if="v$.client.email.$error">
-                <p
-                  class="text-red-700"
-                  v-for="error of v$.client.email.$errors"
-                  :key="error.$uid"
-                >
+                <p class="text-red-700" v-for="error of v$.client.email.$errors" :key="error.$uid">
                   {{ error.$message }}!
                 </p>
               </span>
@@ -247,21 +217,11 @@ export default {
             <label class="block">
               <span class="text-gray-700">Phone Number</span>
               <span style="color: #ff0000">*</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
-                v-model="client.phoneNumber.primary"
-              />
-              <span
-                class="text-black"
-                v-if="v$.client.phoneNumber.primary.$error"
-              >
-                <p
-                  class="text-red-700"
-                  v-for="error of v$.client.phoneNumber.primary.$errors"
-                  :key="error.$uid"
-                >
+                pattern="[0-9]{3}[0-9]{3}[0-9]{4}" v-model="client.phoneNumber.primary" />
+              <span class="text-black" v-if="v$.client.phoneNumber.primary.$error">
+                <p class="text-red-700" v-for="error of v$.client.phoneNumber.primary.$errors" :key="error.$uid">
                   {{ error.$message }}!
                 </p>
               </span>
@@ -271,41 +231,32 @@ export default {
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Alternative Phone Number</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                pattern="[0-9]{3}[0-9]{3}[0-9]{4}"
-                v-model="client.phoneNumber.alternate"
-              />
+                pattern="[0-9]{3}[0-9]{3}[0-9]{4}" v-model="client.phoneNumber.alternate" />
             </label>
           </div>
         </div>
 
         <!-- grid container -->
-        <div
-          class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-        >
+        <div class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
           <h2 class="text-2xl font-bold">Address Details</h2>
           <!-- form field -->
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Address Line 1</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="client.address.line1"
-              />
+                v-model="client.address.line1" />
             </label>
           </div>
           <!-- form field -->
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Address Line 2</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="client.address.line2"
-              />
+                v-model="client.address.line2" />
             </label>
           </div>
           <!-- form field -->
@@ -313,11 +264,9 @@ export default {
             <label class="block">
               <span class="text-gray-700">City</span>
               <span style="color: #ff0000">*</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="client.address.city"
-              />
+                v-model="client.address.city" />
             </label>
           </div>
           <div></div>
@@ -325,55 +274,37 @@ export default {
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">County</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="client.address.county"
-              />
+                v-model="client.address.county" />
             </label>
           </div>
           <!-- form field -->
           <div class="flex flex-col">
             <label class="block">
               <span class="text-gray-700">Zip Code</span>
-              <input
-                type="text"
+              <input type="text"
                 class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                v-model="client.address.zip"
-              />
+                v-model="client.address.zip" />
             </label>
           </div>
           <div></div>
         </div>
 
         <!-- grid container -->
-        <div
-          class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-        >
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
           <div class="flex justify-between mt-10 mr-20">
-            <button
-              @click="updateClient"
-              type="submit"
-              class="bg-green-700 text-white rounded"
-            >
+            <button @click="updateClient" type="submit" class="bg-green-700 text-white rounded">
               Update Client
             </button>
           </div>
           <div class="flex justify-between mt-10 mr-20">
-            <button
-              @click="deregisterClient"
-              type="submit"
-              class="bg-red-700 text-white rounded"
-            >
+            <button @click="deregisterClient" type="submit" class="bg-red-700 text-white rounded">
               Delete Client
             </button>
           </div>
           <div class="flex justify-between mt-10 mr-20">
-            <button
-              type="reset"
-              class="border border-red-700 bg-white text-red-700 rounded"
-              @click="$router.back()"
-            >
+            <button type="reset" class="border border-red-700 bg-white text-red-700 rounded" @click="$router.back()">
               Go back
             </button>
           </div>
@@ -382,9 +313,7 @@ export default {
         <hr class="mt-10 mb-10" />
 
         <!-- Client Event Information -->
-        <div
-          class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-        >
+        <div class="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
           <h2 class="text-2xl font-bold">Events for Client</h2>
 
           <div class="flex flex-col col-span-2">
@@ -397,11 +326,7 @@ export default {
               </thead>
               <tbody class="divide-y divide-gray-300">
                 <!-- allow click through to event details -->
-                <tr
-                  @click="editEvent(event._id)"
-                  v-for="event in eventsRegistered"
-                  :key="event._id"
-                >
+                <tr @click="editEvent(event._id)" v-for="event in eventsRegistered" :key="event._id">
                   <td class="p-2 text-left">{{ event.name }}</td>
                   <td class="p-2 text-left">
                     {{ formattedDate(event.date) }}
@@ -415,21 +340,10 @@ export default {
             <!-- fixed weird selection duplication bug -->
             <VueMultiselect
               class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              v-model="eventsSelected"
-              :options="eventsAll"
-              :custom-label="nameWithDate"
-              :multiple="true"
-              :close-on-select="true"
-              placeholder="Select Events to be added"
-              label="date"
-              track-by="name"
-            />
+              v-model="eventsSelected" :options="eventsAll" :custom-label="nameWithDate" :multiple="true"
+              :close-on-select="true" placeholder="Select Events to be added" label="date" track-by="name" />
             <div class="flex justify-between">
-              <button
-                @click="addToEvent"
-                type="submit"
-                class="mt-5 bg-red-700 text-white rounded"
-              >
+              <button @click="addToEvent" type="submit" class="mt-5 bg-red-700 text-white rounded">
                 Add Client to Selected Events
               </button>
             </div>
