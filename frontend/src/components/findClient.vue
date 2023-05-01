7f845328938d13634a5664839a345c58d95d88b9
VirgilHemlin <123130655+VirgilHemlin@users.noreply.github.com>
2023-04-30 21:23:41 -0500

    Integrated with backend ajax crud operations.

--- frontend/src/components/findClient.vue
+++ frontend/src/components/findClient.vue
@@ -1,8 +1,15 @@
 <script>
+import useVuelidate from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
 import axios from 'axios'
-const apiURL = "https://dataplatform-api.azurewebsites.net/"; 
+const apiURL = import.meta.env.VITE_ROOT_API
+import { useLoggedInUserStore } from "@/store/loggedInUser";
 
 export default {
+  setup() {
+    const user = useLoggedInUserStore();
+    return { v$: useVuelidate({ $autoDirty: true }), user }
+  },
   data() {
     return {
       queryData: [],
@@ -28,10 +35,11 @@ export default {
         this.queryData = res.data
       })
     },
-    // abstract get clients call
+    // Get clients call
     getClients() {
       axios.get(`${apiURL}/clients`).then((res) => {
         this.queryData = res.data
+        console.log(res.data)
       })
       window.scrollTo(0, 0)
     },
@@ -46,7 +54,9 @@ export default {
       this.getClients()
     },
     editClient(clientID) {
-      this.$router.push({ name: 'updateclient', params: { id: clientID } })
+      if (this.user.isEditor) {
+        this.$router.push({ name: 'updateclient', params: { id: clientID } })
+      }
     }
   }
 }
@@ -54,78 +64,52 @@ export default {
 <template>
   <main>
     <div>
-      <h1
-        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
-      >
+      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">
         Find Client
       </h1>
     </div>
     <div class="px-10 pt-20">
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-      >
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
         <h2 class="text-2xl font-bold">Search Client By</h2>
         <!-- Displays Client Name search field -->
         <div class="flex flex-col">
           <select
             class="rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            v-model="searchBy"
-          >
+            v-model="searchBy">
             <option value="Client Name">Client Name</option>
             <option value="Client Number">Client Number</option>
           </select>
         </div>
         <div class="flex flex-col" v-if="searchBy === 'Client Name'">
           <label class="block">
-            <input
-              type="text"
+            <input type="text"
               class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              v-model="firstName"
-              v-on:keyup.enter="handleSubmitForm"
-              placeholder="Enter first name"
-            />
+              v-model="firstName" v-on:keyup.enter="handleSubmitForm" placeholder="Enter first name" />
           </label>
         </div>
         <div class="flex flex-col" v-if="searchBy === 'Client Name'">
           <label class="block">
-            <input
-              type="text"
+            <input type="text"
               class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-              v-model="lastName"
-              v-on:keyup.enter="handleSubmitForm"
-              placeholder="Enter last name"
-            />
+              v-model="lastName" v-on:keyup.enter="handleSubmitForm" placeholder="Enter last name" />
           </label>
         </div>
         <!-- Displays Client Number search field -->
         <div class="flex flex-col" v-if="searchBy === 'Client Number'">
           <input
             class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-            type="text"
-            v-model="phoneNumber"
-            v-on:keyup.enter="handleSubmitForm"
-            placeholder="Enter Client Phone Number"
-          />
+            type="text" v-model="phoneNumber" v-on:keyup.enter="handleSubmitForm"
+            placeholder="Enter Client Phone Number" />
         </div>
       </div>
-      <div
-        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-      >
+      <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
         <div></div>
         <div></div>
         <div class="mt-5 grid-cols-2">
-          <button
-            class="mr-10 border border-red-700 bg-white text-red-700 rounded"
-            @click="clearSearch"
-            type="submit"
-          >
+          <button class="mr-10 border border-red-700 bg-white text-red-700 rounded" @click="clearSearch" type="submit">
             Clear Search
           </button>
-          <button
-            class="bg-red-700 text-white rounded"
-            @click="handleSubmitForm"
-            type="submit"
-          >
+          <button class="bg-red-700 text-white rounded" @click="handleSubmitForm" type="submit">
             Search Client
           </button>
         </div>
@@ -134,9 +118,7 @@ export default {
 
     <hr class="mt-10 mb-10" />
     <!-- Display Found Data -->
-    <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
-    >
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
       <div class="ml-10">
         <h2 class="text-2xl font-bold">List of Clients</h2>
         <h3 class="italic">Click table row to edit/display an entry</h3>
@@ -151,11 +133,7 @@ export default {
             </tr>
           </thead>
           <tbody class="divide-y divide-gray-300">
-            <tr
-              @click="editClient(client._id)"
-              v-for="client in queryData"
-              :key="client._id"
-            >
+            <tr @click="editClient(client._id)" v-for="client in queryData" :key="client._id">
               <td class="p-2 text-left">
                 {{ client.firstName + ' ' + client.lastName }}
               </td>
